05fe463e34739964e9a5a994a533f23a8ba22951
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-01 04:32:53 -0600

    Add full proxy project to repository

---
+++ Chapter4/FullTrustProxy/GetWebApplications.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.UserCode;
+using Microsoft.SharePoint.Administration;
+
+namespace FullTrustProxy
+{
+    public class GetWebApplications : SPProxyOperation
+    {
+        public override object Execute(SPProxyOperationArgs args)
+        {
+            var webApplications = new List<string>();
+            var webService = SPFarm.Local.Servers.GetValue<SPWebService>();
+
+            foreach (var webApplication in webService.WebApplications)
+                webApplications.Add(webApplication.DisplayName);
+
+            return webApplications;
+        }
+    }
+}
