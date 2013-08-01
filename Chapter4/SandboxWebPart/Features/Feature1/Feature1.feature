7350f30f3879e6cdc78e9d011b6e53c7f4d5b3e6
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-01 06:08:02 -0600

    Create sandboxed solution that uses the full-trust proxy

---
+++ Chapter4/SandboxWebPart/Features/Feature1/Feature1.feature
@@ -0,0 +1,13 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature title="SandboxWebPart Feature1"
+ dslVersion="1.0.0.0"
+ Id="4ec69b2f-33d8-4d1c-b067-b3abce6cff92"
+ featureId="4ec69b2f-33d8-4d1c-b067-b3abce6cff92"
+ deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$"
+ description="My Visual Web Part Feature"
+ scope="Site"
+ xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="9c1a75f6-6aa6-4a91-b5b6-287738681814" />
+  </projectItems>
+</feature>
---
+++ Chapter4/SandboxWebPart/VisualWebPart1/VisualWebPart1.ascx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint.Utilities;
+using System.Collections.Generic;
+
+namespace SandboxWebPart.VisualWebPart1
+{
+    [ToolboxItemAttribute(false)]
+    public partial class VisualWebPart1 : WebPart
+    {
+        // Uncomment the following SecurityPermission attribute only when doing Performance Profiling using
+        // the Instrumentation method, and then remove the SecurityPermission attribute when the code is ready
+        // for production. Because the SecurityPermission attribute bypasses the security check for callers of
+        // your constructor, it's not recommended for production purposes.
+        // [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Assert, UnmanagedCode = true)]
+        public VisualWebPart1()
+        {
+        }
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+            InitializeControl();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected override void RenderContents(System.Web.UI.HtmlTextWriter writer)
+        {
+            base.RenderContents(writer);
+
+            var operationAssemblyName = "FullTrustProxy, Version=1.0.0.0, Culture=neutral, PublicKeyToken=adfe4e9f5594a2b3";
+            var operationTypeName = "FullTrustProxy.GetWebApplications";
+
+            var webApplications = SPUtility.ExecuteRegisteredProxyOperation(operationAssemblyName, operationTypeName, null) as IList<string>;
+
+            foreach (var webApplication in webApplications)
+                writer.Write(webApplication + "<br />");
+        }
+    }
+}
