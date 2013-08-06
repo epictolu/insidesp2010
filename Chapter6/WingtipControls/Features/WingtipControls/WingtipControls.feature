455e6380d1cd1d4f3cf2ea6944e888706bbaf677
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-06 02:58:16 -0600

    Add sample code for a custom control

---
+++ Chapter6/WingtipControls/Features/WingtipControls/WingtipControls.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="c0e33123-04eb-49b1-8429-5581b83730b8" featureId="c0e33123-04eb-49b1-8429-5581b83730b8" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="WingtipControls Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="5505a474-6d36-45fa-bfed-78aed1790a55" />
+  </projectItems>
+</feature>
---
+++ Chapter6/WingtipControls/WingtipControls/WingtipCustomControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+using System.Web.UI;
+
+namespace WingtipControls.WingtipControls
+{
+    // This class is for demonstration purposes. It is not used anywhere in this solution.
+    public class WingtipCustomControl : Control
+    {
+        public string UserGreeting { get; set; }
+
+        protected override void Render(HtmlTextWriter writer)
+        {
+            writer.AddStyleAttribute(HtmlTextWriterStyle.Color, "Blue");
+            writer.AddStyleAttribute(HtmlTextWriterStyle.FontSize, "18pt");
+            writer.RenderBeginTag(HtmlTextWriterTag.Div);
+            writer.Write(UserGreeting);
+            writer.RenderEndTag();
+        }
+    }
+}
