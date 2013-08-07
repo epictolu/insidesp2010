16817ab988eac0db833164ad0497a6671ab9a2d9
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-07 11:27:57 -0600

    implement WebPart2

---
+++ Chapter6/WingtipWebParts/WingtipWebParts/WebPart2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using System.Drawing;
+
+namespace WingtipWebParts.WingtipWebParts.WebPart2
+{
+    [ToolboxItemAttribute(false)]
+    public class WebPart2 : WebPart
+    {
+        protected Label label;
+
+        protected override void CreateChildControls()
+        {
+            label = new Label();
+            label.Text = "Hello Child Controls";
+            label.Font.Size = new FontUnit(18);
+            label.ForeColor = Color.Blue;
+
+            Controls.Add(label);
+        }
+    }
+}
