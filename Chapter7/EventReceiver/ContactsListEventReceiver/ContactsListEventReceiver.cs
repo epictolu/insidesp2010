a6771634eabd7feacc05bf571a1f994d0b1b5b5e
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-14 19:33:00 -0600

    Finished chapter 7

---
+++ Chapter7/EventReceiver/ContactsListEventReceiver/ContactsListEventReceiver.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Security.Permissions;
+using Microsoft.SharePoint;
+using Microsoft.SharePoint.Utilities;
+using Microsoft.SharePoint.Workflow;
+
+namespace EventReceiver.ContactsListEventReceiver
+{
+    /// <summary>
+    /// List Item Events
+    /// </summary>
+    public class ContactsListEventReceiver : SPItemEventReceiver
+    {
+        /// <summary>
+        /// An item is being added.
+        /// </summary>
+        public override void ItemAdding(SPItemEventProperties properties)
+        {
+            base.ItemAdding(properties);
+        }
+
+        /// <summary>
+        /// An item is being updated.
+        /// </summary>
+        public override void ItemUpdating(SPItemEventProperties properties)
+        {
+            base.ItemUpdating(properties);
+        }
+
+        /// <summary>
+        /// An item was added
+        /// </summary>
+        public override void ItemAdded(SPItemEventProperties properties)
+        {
+            base.ItemAdded(properties);
+        }
+    }
+}
---
+++ Chapter7/EventReceiver/Features/ContactsListEventReceiver/ContactsListEventReceiver.feature
@@ -0,0 +1,6 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="3642209b-bbfb-4909-9cdc-9bf59e2a91f9" featureId="3642209b-bbfb-4909-9cdc-9bf59e2a91f9" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="EventReceiver Feature1" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="0156061d-66a2-430a-bd4d-25abf9c64e42" />
+  </projectItems>
+</feature>
