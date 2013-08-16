8d6d129a4269f55370e076a1a685287f53f568cc
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-16 12:01:25 -0600

    Implement SocialSecurityNumber custom field type

---
+++ Chapter8/WingtipFieldTypes/SocialSecurityNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.SharePoint;
+using System.Text.RegularExpressions;
+using Microsoft.SharePoint.WebControls;
+
+namespace WingtipFieldTypes
+{
+    public class SocialSecurityNumber : SPFieldText
+    {
+        public SocialSecurityNumber(SPFieldCollection fields, string fieldName) : base(fields, fieldName) { }
+        public SocialSecurityNumber(SPFieldCollection fields, string typeName, string displayName) : base(fields, typeName, displayName) { }
+
+        public override string GetValidatedString(object value)
+        {
+            var userInput = value.ToString();
+
+            var regex = @"^\d{3}-\d{3}-\d{3}$";
+
+            if (!string.IsNullOrEmpty(userInput) && !Regex.IsMatch(userInput, regex))
+                throw new SPFieldValidationException("SSN must be form of 123-456-789");
+
+            return base.GetValidatedString(value);
+        }
+
+        public override BaseFieldControl FieldRenderingControl
+        {
+            get
+            {
+                BaseFieldControl fldControl = new SocialSecurityNumberFieldControl();
+                fldControl.FieldName = InternalName;
+
+                return fldControl;
+            }
+        }
+    }
+}
---
+++ Chapter8/WingtipFieldTypes/SocialSecurityNumberFieldControl.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.SharePoint.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace WingtipFieldTypes
+{
+    public class SocialSecurityNumberFieldControl : BaseFieldControl
+    {
+        protected TextBox userInput;
+
+        protected override string DefaultTemplateName
+        {
+            get { return "SocialSecurityNumberRenderingTemplate"; }
+        }
+
+        protected override void CreateChildControls()
+        {
+            base.CreateChildControls();
+
+            userInput = (TextBox) TemplateContainer.FindControl("txtUserInput");
+        }
+
+        public override object Value
+        {
+            get
+            {
+                EnsureChildControls();
+                return userInput.Text;
+            }
+            set
+            {
+                EnsureChildControls();
+                userInput.Text = ItemFieldValue as string;
+            }
+        }
+    }
+}
