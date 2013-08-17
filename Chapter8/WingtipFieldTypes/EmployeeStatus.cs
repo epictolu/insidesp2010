2c1337580e56c33094e1b4c5966611e4096da1ca
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-16 18:54:03 -0600

    Implement EmployeeStatus custom field type

---
+++ Chapter8/WingtipFieldTypes/EmployeeStatus.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.SharePoint;
+using Microsoft.SharePoint.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WingtipFieldTypes
+{
+    public class EmployeeStatus : SPFieldText
+    {
+        public EmployeeStatus(SPFieldCollection fields, string fieldName) : base(fields, fieldName) { }
+        public EmployeeStatus(SPFieldCollection fields, string typeName, string displayName) : base(fields, typeName, displayName) { }
+
+        public override BaseFieldControl FieldRenderingControl
+        {
+            get
+            {
+                var fieldControl = new EmployeeStatusFieldControl();
+                fieldControl.FieldName = InternalName;
+
+                return fieldControl;
+            }
+        }
+    }
+}
---
+++ Chapter8/WingtipFieldTypes/EmployeeStatusFieldControl.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.SharePoint.WebControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.UI.WebControls;
+
+namespace WingtipFieldTypes
+{
+    public class EmployeeStatusFieldControl : BaseFieldControl
+    {
+        protected RadioButtonList lstEmployeeStatus;
+
+        protected override string DefaultTemplateName
+        {
+            get
+            {
+                return "EmployeeStatusRenderingTemplate";
+            }
+        }
+
+        protected override void CreateChildControls()
+        {
+            base.CreateChildControls();
+
+            lstEmployeeStatus = TemplateContainer.FindControl("lstEmployeeStatus") as RadioButtonList;
+
+            lstEmployeeStatus.Items.Clear();
+            lstEmployeeStatus.Items.Add("Full-Time Employee");
+            lstEmployeeStatus.Items.Add("Part-Time Employee");
+
+            if ((bool) Field.GetCustomProperty("AllowContractors"))
+                lstEmployeeStatus.Items.Add("Contractor");
+            if ((bool) Field.GetCustomProperty("AllowInterns"))
+                lstEmployeeStatus.Items.Add("Intern");
+        }
+    }
+}
