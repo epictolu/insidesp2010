7ba915b059ec6edfc1163804ae1fad77f83e28b8
Tolu Adesegha <toluwani.adesegha@gmail.com>
2013-08-20 11:42:33 -0600

    Implement site provisioning provider

---
+++ Chapter8/WingtipProvisioningProvider/WingtipProvisioningProvider.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.SharePoint;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WingtipProvisioningProvider
+{
+    // This project gives an error about security validation ... 
+    public class WingtipProvisioningProvider : SPWebProvisioningProvider
+    {
+        public override void Provision(SPWebProvisioningProperties props)
+        {
+            props.Web.ApplyWebTemplate("STS#1");
+
+            SPSecurity.RunWithElevatedPrivileges(() => 
+                {
+                    using (var siteCollection = new SPSite(props.Web.Site.ID))
+                    {
+                        using (var site = siteCollection.OpenWeb(props.Web.ID))
+                        {
+                            site.Title = "Provisioned using Provisioning Provider";
+                            site.Update();
+                        }
+                    }
+                });
+        }
+    }
+}
